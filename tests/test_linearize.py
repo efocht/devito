fb82d932ea6a06d7c1d4aa7aacc45b623f16766d
Fabio Luporini <f.luporini12@imperial.ac.uk>
2021-07-12 18:11:09 +0200

    aa

--- tests/test_linearize.py
+++ tests/test_linearize.py
@@ -117,3 +117,6 @@ def test_codegen_quality():
     # Only four access macros necessary, namely `uL0`, `aL0`, `bufL0`, `bufL1` (the
     # other three obviously are _POSIX_C_SOURCE, START_TIMER, STOP_TIMER)
     assert len(op._headers) == 7
+
+    exprs = FindNodes(Expression).visit(op._func_table['compute0'].root)
+    assert all('const int' in str(i) for i in exprs[:-1])
